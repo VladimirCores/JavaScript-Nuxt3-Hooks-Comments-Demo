54d84339a2cba3bbdfdae8d343d90801afdf03a8
Vladimir Cores (Minkin) <vladimir.minkin@gmail.com>
2021-12-02 00:44:44 +0300

    Turn on SSR because in disabled mode build fails

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  ssr: false,
+  ssr: true,
   publicRuntimeConfig: {
     API_URL: process.env.API_URL,
   },
