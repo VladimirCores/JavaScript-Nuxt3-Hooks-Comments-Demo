a5902a89b505df473c92edea8e371a3d9adf0b9f
Vladimir Cores (Minkin) <vladimir.minkin@gmail.com>
2021-12-02 03:14:42 +0300

    .nvmrc was added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  ssr: true,
+  ssr: false,
   bridge: false,
   target: 'static',
   nitro: {
